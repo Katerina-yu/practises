c0612cf34152ab08eaa7e10d2cdfb53ee57a112c
Katerina-yu <2461643402@qq.com>
2020-04-03 22:24:30 +0800

    上周最佳搭配展示

---
+++ project5/js/index.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el : "#mask",
+    data : {
+        imgArr : [
+            "../picture/0.png",
+            "../picture/1.png",
+            "../picture/2.png",
+            "../picture/3.png"
+        ],
+        index : 0
+    },
+    methods : {
+        prev : function() {
+            this.index--;
+        },
+        next : function() {
+            this.index++;
+        }
+    }
+})
